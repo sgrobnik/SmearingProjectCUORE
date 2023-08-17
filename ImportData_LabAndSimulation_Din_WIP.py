c998584f2dc7f6d32d27acc16ac1065949cd2206
Jorge Torres <jorge.torresespinosa@yale.edu>
2023-08-17 13:55:57 -0400

    Changed linear smearing model as Samantha and Jorge suggested, added helper notebooks for importing and normalizing data, general cleaning up and more commenting.

---
+++ ImportData_LabAndSimulation_Din_WIP.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+##################################
+# Din-Ammar Tolj - Y25
+# Maruyama Group, Summer 2023
+# 
+# This scrip 
+#
+#
+#
+#
+###########################
+
+
+
+class ImportData:
+    
